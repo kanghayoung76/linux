kanghayoung76/linux

commit 7a48343f34b8a166d27a69a6b9c4c763111ccf1e

riscv: Cleanup setup_bootmem()
After the following patches,

  commit d1aeda951e71 ("RISC-V: Fix usage of memblock_enforce_memory_limit")
  commit 083668366c73 ("RISC-V: Remove any memblock representing unusable memory area")
  commit 0c04cf952039 ("arch, drivers: replace for_each_membock() with for_each_mem_range()")

some logic is useless, kill the mem_start/start/end and unneeded code.

Reviewed-by: Atish Patra <atish.patra@wdc.com>
Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

arch/riscv/mm/init.c
@@ -108,33 +108,16 @@ void __init mem_init(void)
 
 void __init setup_bootmem(void)
 {
-	phys_addr_t mem_start = 0;
-	phys_addr_t start, dram_end, end = 0;
 	phys_addr_t vmlinux_end = __pa_symbol(&_end);
 	phys_addr_t vmlinux_start = __pa_symbol(&_start);
+	phys_addr_t dram_end = memblock_end_of_DRAM();
 	phys_addr_t max_mapped_addr = __pa(~(ulong)0);
-	u64 i;
 
-	/* Find the memory region containing the kernel */
-	for_each_mem_range(i, &start, &end) {
-		phys_addr_t size = end - start;
-		if (!mem_start)
-			mem_start = start;
-		if (start <= vmlinux_start && vmlinux_end <= end)
-			BUG_ON(size == 0);
-	}
-
-	/*
-	 * The maximal physical memory size is -PAGE_OFFSET.
-	 * Make sure that any memory beyond mem_start + (-PAGE_OFFSET) is removed
-	 * as it is unusable by kernel.
-	 */
+	/* The maximal physical memory size is -PAGE_OFFSET. */
 	memblock_enforce_memory_limit(-PAGE_OFFSET);
 
 	/* Reserve from the start of the kernel to the end of the kernel */
 	memblock_reserve(vmlinux_start, vmlinux_end - vmlinux_start);
-
-	dram_end = memblock_end_of_DRAM();
 
 	/*
 	 * memblock allocator is not aware of the fact that last 4K bytes of

commit 74278c2a83fd5276590d8cef12574d52d086b0e2

riscv: Fix 32-bit RISC-V boot failure
Commit 7a48343f34b8 ("riscv: Cleanup setup_bootmem()") adjusted
the calling sequence in setup_bootmem(), which invalidates the fix
commit d1aeda951e71 ("RISC-V: Fix usage of memblock_enforce_memory_limit")
did for 32-bit RISC-V unfortunately.

So now 32-bit RISC-V does not boot again when testing booting kernel
on QEMU 'virt' with '-m 2G', which was exactly what the original
commit d1aeda951e71 ("RISC-V: Fix usage of memblock_enforce_memory_limit")
tried to fix.

Fixes: 7a48343f34b8 ("riscv: Cleanup setup_bootmem()")
Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

arch/riscv/mm/init.c
@@ -123,7 +123,7 @@ void __init setup_bootmem(void)
 {
 	phys_addr_t vmlinux_end = __pa_symbol(&_end);
 	phys_addr_t vmlinux_start = __pa_symbol(&_start);
-	phys_addr_t dram_end = memblock_end_of_DRAM();
+	phys_addr_t dram_end;
 	phys_addr_t max_mapped_addr = __pa(~(ulong)0);
 
 #ifdef CONFIG_XIP_KERNEL
@@ -145,6 +145,8 @@ void __init setup_bootmem(void)
 	vmlinux_end = (vmlinux_end + PMD_SIZE - 1) & PMD_MASK;
 #endif
 	memblock_reserve(vmlinux_start, vmlinux_end - vmlinux_start);
+
+	dram_end = memblock_end_of_DRAM();
 
 	/*
 	 * memblock allocator is not aware of the fact that last 4K bytes of