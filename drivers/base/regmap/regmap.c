kanghayoung76/linux

commit 5e1f2889dd662c884a2db2bb0a32a1dcf29d31ab

regmap: Check for register readability before checking cache during read
Ensure that we don't return a spurious cache hit for unreadable registers
(eg, with the flat cache which doesn't understand sparseness) by checking
for readability before we do a cache lookup.

Signed-off-by: Mark Brown <broonie@kernel.org>
Link: https://lore.kernel.org/r/20230613-b4-regmap-check-readability-before-cache-v1-1-b144c0b01ed9@kernel.org
Signed-off-by: Mark Brown <broonie@kernel.org>

drivers/base/regmap/regmap.c
@@ -2897,6 +2897,9 @@ static int _regmap_read(struct regmap *map, unsigned int reg,
 	int ret;
 	void *context = _regmap_map_get_context(map);
 
+	if (!regmap_readable(map, reg))
+		return -EIO;
+
 	if (!map->cache_bypass) {
 		ret = regcache_read(map, reg, val);
 		if (ret == 0)
@@ -2905,9 +2908,6 @@ static int _regmap_read(struct regmap *map, unsigned int reg,
 
 	if (map->cache_only)
 		return -EBUSY;
-
-	if (!regmap_readable(map, reg))
-		return -EIO;
 
 	ret = map->reg_read(context, reg, val);
 	if (ret == 0) {

commit 3a40b992a2a4f8a7a81816e0164578d3b2e03444

regmap: Drop early readability check
We have some drivers that have a use case for cached write only
registers, doing read/modify/writes on read only registers in order to
work more easily with bitfields.  Go back to trying the cache before we
check if we can read from the device.

Fixes: 5e1f2889dd662 ("regmap: Check for register readability before checking cache during read")
Reported-by: Konrad Dybcio <konradybcio@kernel.org>
Signed-off-by: Mark Brown <broonie@kernel.org>
Link: https://lore.kernel.org/r/20230615-regmap-drop-early-readability-v1-1-8135094362de@kernel.org
Signed-off-by: Mark Brown <broonie@kernel.org>

drivers/base/regmap/regmap.c
@@ -2897,9 +2897,6 @@ static int _regmap_read(struct regmap *map, unsigned int reg,
 	int ret;
 	void *context = _regmap_map_get_context(map);
 
-	if (!regmap_readable(map, reg))
-		return -EIO;
-
 	if (!map->cache_bypass) {
 		ret = regcache_read(map, reg, val);
 		if (ret == 0)
@@ -2908,6 +2905,9 @@ static int _regmap_read(struct regmap *map, unsigned int reg,
 
 	if (map->cache_only)
 		return -EBUSY;
+
+	if (!regmap_readable(map, reg))
+		return -EIO;
 
 	ret = map->reg_read(context, reg, val);
 	if (ret == 0) {