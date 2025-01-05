824aaa66d32499e8d8c84fa040048f465e6975db
kanghayoung76 <rkdgkdud@pusan.ac.kr>
2025-01-05 22:28:43 +0900

    no genesis_copy_user

--- arch/riscv/genesis/inner.c
+++ arch/riscv/genesis/inner.c
@@ -34,13 +34,6 @@ void _genesis_shadow_prol(unsigned long ra)
                     : "r" (ra)
         );
 
-	unsigned long gp;
-                asm volatile (
-                        "mv %0, gp"
-                        : "=r" (gp)
-                );
-                printk("gp -16 : 0x%lx\n",gp);
-	
 /*
 	gp = __virt_to_shadow(gp);
 
@@ -197,8 +190,6 @@ unsigned long __genesis inner_handler(unsigned long svc_num,
 		_genesis_write_satp(arg0);
 		break;
 	case GENESIS_SHADOW_PROL:
-		printk("arg0 : 0x%lx\n",arg0);
-		printk("arg1 : 0x%lx\n",arg1);
 		_genesis_shadow_prol(arg0);
 		break;
 	case GENESIS_WRITE_CSR:
--- mm/debug_vm_pgtable.c
+++ mm/debug_vm_pgtable.c
@@ -444,7 +444,7 @@ static void __init pmd_huge_tests(struct pgtable_debug_args *args)
 	WRITE_ONCE(*args->pmdp, __pmd(0));
 #else
 	_genesis_entry(/*svc_num*/ GENESIS_SET_PMD,
-		       /*arg0*/ (unsigned long)args->pmdp,
+		       /*arg0*/ (unsigned long)&(*args->pmdp),
 		       /*arg1*/ pmd_val(__pmd(0)));
 #endif
 	WARN_ON(!pmd_set_huge(args->pmdp, __pfn_to_phys(args->fixed_pmd_pfn), args->page_prot));
